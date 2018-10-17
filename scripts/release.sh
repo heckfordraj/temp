0b62dc9d46825ab3d7e9247155a9e097e1ba2dbc
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-10-17 16:48:43 +0100

    feat: hkfd-ci user test

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+git config --global user.email "44237005+hkfd-ci@users.noreply.github.com"
+git config --global user.name "Hkfd CI"
+
 git checkout master
 git reset --hard
 git remote add release https://${GH_TOKEN}@github.com/heckfordraj/temp.git
